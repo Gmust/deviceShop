28de56eb502b3da97fcf472c2ec75a6b3f1d8c4f
Reflexive <4orgood@gmail.com>
2022-07-29 19:18:21 +0200

    Added router

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ require ('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./db')
-
+const models = require('./models/models')
+const router = require('./routes/index')
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use('/api', router);
 
 const PORT = process.env.PORT;
 
@@ -15,6 +17,7 @@ const PORT = process.env.PORT;
 const appStart = async ()=> {
     try {
         await sequelize.authenticate();
+        await sequelize.sync();
         app.listen(PORT, () => `Serer started on port ${PORT}`)
     } catch (e) {
         console.log(e);
